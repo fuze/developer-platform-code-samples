733ed90f1000e4bb7a3c3a62bdf67d55eab06031
dwmccorm <david.william.mccormack@gmail.com>
2020-03-25 14:38:49 -0400

    Create config.js
    Inital commit

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {}
+
+// environment configuration
+config.fuzeApiBaseUrl = 'https://developer.fuze.com'
+config.authToken = '<ACCESS TOKEN>'
+config.httpTimeoutMillis = 5000
+
+module.exports = config;
